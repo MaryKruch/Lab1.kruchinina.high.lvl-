3c4ea257b71f0b07a7e7bb333183329bf1a622fd
NIK <NIK@DESKTOP-MH7JFSC>
2023-09-28 00:31:12 +0200

    Add project files.

---
+++ Lab1.kruchinina.high.lvl,/Program.cs
@@ -0,0 +1,7 @@
+﻿//Вариант 4. Уровень высокий
+Console.Write("Ведите y:");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Ведите x:");
+double x = double.Parse(Console.ReadLine());
+double F = (Math.Sqrt(Math.Pow((2 + y), 2) + Math.Pow(Math.Sqrt(Math.Sin(y + 5)), 1 / 7.0))) / Math.Log(x + 1) - Math.Pow(y, 3);
+Console.WriteLine($"F = {F:F2}");
